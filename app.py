a2bc3b2f1562ba76a3ddbc96268f78abf02cb5b9
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 11:01:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.markdown("### please input password")
 UID = st.text_input("username")
 passwd = st.text_input("password")
 if st.button("conform"):
-  if UID == 123:
-    if passwd == 123:
+  if UID == "123":
+    if passwd == "123":
       st.success("ture")
