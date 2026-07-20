6a0c3c6810d188bd3bb29a4f20f9e0617eef66b5
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 10:34:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
-st.title("my webapp")
-st.header("todat's question")
-st.subheader("input")
-st.write("hello world")
-st.write("write anything")
+st.markdown("#AI챗봇")
+st.markdown("---")
+st.markdown("##질문을 하시면 AI친구가 응답합니다")
+st.markdown("* **제작언어**: `python`")
+st.markdown("* **프레임워크**:`streamlit`")
+st.markdown("###`최근대화`")
+st.markdown("~~질문을 자유롭게 하세요.~~")
