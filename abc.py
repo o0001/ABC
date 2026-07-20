f2f8534dfd89ba366f84336f9d6f168b62ce7e30
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 14:01:12 +0900

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+def reset_all():
+  st.session_state.user = ""
+  st.session_state.weather = "맑음"
+  st.session_state.top_type = "후드티"
+  st.session_state.top_color = "밝음"
+  st.session_state.bottom_type = "청바지"
+  st.session_state.bottom_color = "슬림"
+  st.session_state.shoes = "스니커즈"
+  st.session_state.acc = []
 with st.sidebar:
   st.header("프로필")
   user_name = st.text_input("닉네임")
