f0757d7ac71ae795a61c1b5af81ff1c96579b623
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 10:55:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.markdown("# login page")
 st.markdown("### please input password")
 UID = st.text_input("username")
 passwd = st.text_input("password")
-
+st.button("conform")
 st.button("forgot your password? click here.")
