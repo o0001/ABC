a228cfe86c67e16b27ce1172015939690cefd942
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 14:52:14 +0900

    Update work.py

--- work.py
+++ work.py
@@ -15,15 +15,13 @@ with col1:
     if st.button("시작"):
         st.session_state.start_time = time.time() # 현재 시각 기록
         st.session_state.end_time = 0          # 종료 시간 초기화
-A = time.time()
-B = st.session_state.start_time-A
-st.write(f"{B}")
 with col2:
     if st.button("종료"):
         if st.session_state.start_time != 0:
             st.session_state.end_time = time.time()
             # 걸린 시간 계산 (종료 시간 - 시작 시간)
             st.session_state.result = st.session_state.end_time - st.session_state.start_time
+            st.session_state.start_time = 0
         else:
             st.warning("시작 버튼을 먼저 눌러주세요!")
 
