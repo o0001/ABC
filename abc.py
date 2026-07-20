1873af9a30b5bb7884f13f5978acd0ea83fd3cd4
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 11:37:06 +0900

    Update abc.py

--- abc.py
+++ abc.py
@@ -5,7 +5,8 @@ with st.sidebar:
   weather = st.selectbox("오늘 날씨", ["맑음","흐림","비/눈","매우 추움"])
   st.markdown("---")
   st.info(f"반가워요 {user_name}님! 오늘 날씨는'{weather}'이네요.")
-
+st.title("AI 코디 메이커")
+st.write("사이드바에서 날씨를 먼저 선택하고 코디를 시작하세요")
 
 
 
