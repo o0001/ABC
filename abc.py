bc1121c2540cb4be8e0ff7fcae9c01b6bf905ee7
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 11:47:36 +0900

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,14 +1,11 @@
 import streamlit as st
-
-with st.sidebar:
-  st.header("프로필")
-  user_name = st.text_input("닉네임")
-  weather = st.selectbox("오늘 날씨", ["맑음","흐림","비/눈","매우 추움"])
-  st.markdown("---")
-  st.info(f"반가워요 {user_name}님! 오늘 날씨는'{weather}'이네요.")
-
-st.title("👗 AI 코디 메이커")
-st.write("사이드바에서 날씨를 먼저 선택하고 코디를 시작하세요")
-
-
-
+st.header("👕 아이템 조합하기")
+col1, col2 = st.colums(2)
+with col1:
+  st.subheader("상의")
+  top_type = st.radio()
+  top_color = st.select_slider()
+with col2:
+  st.subheader
+  bottom_type
+  bottom_color
