56cc9c2a9058b75150b873575f3d8abb833cab8d
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 11:56:23 +0900

    Update abc.py

--- abc.py
+++ abc.py
@@ -3,9 +3,9 @@ st.header("👕 아이템 조합하기")
 col1, col2 = st.colums(2)
 with col1:
   st.subheader("상의")
-  top_type = st.radio()
-  top_color = st.select_slider()
+  top_type = st.radio("종류",["후드티","셔츠","",""])
+  top_color = st.select_slider("색상 톤",options=["","",""])
 with col2:
-  st.subheader
-  bottom_type = st.radio()
-  bottom_color =st.select_slider()
+  st.subheader("하의")
+  bottom_type = st.radio("종류",["","","",""])
+  bottom_color =st.select_slider("핏(fit)",optiins=["","",""])
