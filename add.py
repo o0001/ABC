de6b713242db35ce05ce74ac5533dd14491bc9f3
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 13:51:36 +0900

    Update add.py

--- add.py
+++ add.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("카운터 앱")
 if 'count' not in st. session_state:
-  st.session_state.count = 0
+  st.session_state.count = 1
 if st.button("증가"):
   st.session_state.count += 1
   st.session_state.A = 10**st.session_state.count
