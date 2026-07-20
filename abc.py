fe64c73836083316ebff70d04cb8000726943e3f
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 14:04:22 +0900

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def reset_all():
-  st.session_state.user = ""
+  st.session_state.user_name = ""
   st.session_state.weather = "맑음"
   st.session_state.top_type = "후드티"
   st.session_state.top_color = "밝음"
