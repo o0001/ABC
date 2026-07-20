816a0351021b7fedc0ca97808b730bb80499bf6b
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 13:02:59 +0900

    Update abc.py

--- abc.py
+++ abc.py
@@ -11,7 +11,16 @@ st.title("👗 AI 코디 메이커")
 st.write("사이드바에서 날씨를 먼저 선택하고 코디를 시작하세요")
 st.header("👕 아이템 조합하기")
 
-col1, col2 = st.tabs(["상의","하의"])
+col1, col2 = st.columns(2)
+with col1:
+  st.subheader("상의")
+  top_type = st.radio("종류",["후드티","셔츠","맨투맨","반팔티셔츠"])
+  top_color = st.select_slider("색상 톤",options=["밝음","무난함","어두움"])
+with col2:
+  st.subheader("하의")
+  bottom_type = st.radio("종류",["청바지","슬랙스","트레이닝 팬츠","반바지"])
+  bottom_color =st.select_slider("핏(fit)",options=["슬림","레귤러","오버핏"])
+tab1, tab2 = st.tabs(["상의","하의"])
 with col1:
   st.subheader("상의")
   top_type = st.radio("종류",["후드티","셔츠","맨투맨","반팔티셔츠"])
