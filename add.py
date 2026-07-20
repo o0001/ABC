4f639f718b853d7d93e20fde77e3e32efdd5c9af
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 13:36:51 +0900

    Update add.py

--- add.py
+++ add.py
@@ -3,5 +3,5 @@ st.title("카운터 앱")
 if 'count' not in st. session_state:
   st.session_state.count = 0
 if st.button("증가"):
-  st.session_state.count += 1
+  st.session_state.count += 1000
 st.markdown(f"## 현제 숫자:`{st.session_state.count}`")
