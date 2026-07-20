25d2dab0a6bd386bc4a214a37c5b31c4fbf946ce
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 11:06:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.markdown("# 앱 UI 만들기")
 user_id = st.text_input("이름", placeholder="이름")
-ai_model = st.radio("사용할 AI 모델을 선택하세요", ["GPT-4", "Claude 3", "Gemini Pro"], horizontal=True)
+ai_model = st.radio("학년", ["1", "2", "3"], horizontal=True)
 
 st.markdown("")
 
