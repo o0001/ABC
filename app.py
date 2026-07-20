7a70a1643c95dabf1438df14930ab0767f0ae9ac
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 11:06:19 +0900

    Fix variable name from ai_model to greade

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.markdown("# 앱 UI 만들기")
 user_id = st.text_input("이름", placeholder="이름")
-ai_model = st.radio("학년", ["1", "2", "3"], horizontal=True)
+greade = st.radio("학년", ["1", "2", "3"], horizontal=True)
 
 st.markdown("")
 
