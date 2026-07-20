4ef14338648736483bf84a975fedc57c712a70e5
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 10:54:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.markdown("# login page")
 st.markdown("### please input password")
-UID = st.text_input
-passwd = st.text_input
+UID = st.text_input("username")
+passwd = st.text_input("password")
 
 st.button("forgot your password? click here.")
