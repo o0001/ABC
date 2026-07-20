8708d6233242111baae183c4010198f77c67e172
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 10:36:22 +0900

    Fix formatting of markdown headers in app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-st.markdown("#AI챗봇")
+st.markdown("# AI챗봇")
 st.markdown("---")
-st.markdown("##질문을 하시면 AI친구가 응답합니다")
+st.markdown("## 질문을 하시면 AI친구가 응답합니다")
 st.markdown("* **제작언어**: `python`")
 st.markdown("* **프레임워크**:`streamlit`")
-st.markdown("###`최근대화`")
+st.markdown("### `최근대화`")
 st.markdown("~~질문을 자유롭게 하세요.~~")
