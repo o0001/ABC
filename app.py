856310d6fb22f53f3844795dd0d705d8dc1def3a
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-20 11:08:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,4 @@ import streamlit as st
 st.markdown("# 앱 UI 만들기")
 user_id = st.text_input("이름", placeholder="이름")
 greade = st.radio("학년", ["1", "2", "3"], horizontal=True)
-
-st.markdown("")
-
-st.markdown("")
-
-st.markdown("")
-
-st.markdown("")
-
-st.markdown("")
-
-st.markdown("")
+cls = st.number_input("반", value=1)
