5177514f0e5d3148e42b2a95c079035616bf633a
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-21 09:53:51 +0900

    Update new.py

--- new.py
+++ new.py
@@ -17,7 +17,7 @@ def edm():
     motto = st.text_input("나의 한 줄 좌우명을 적어주세요")
     if st.button("다짐저장"):
         st.session_state.user_motto = motto
-        st.session_state.motoo_update = 1
+        st.session_state.motto_update = 1
         st.rerun()
 
 def pg1():
