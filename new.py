bf86d6a2d54c22c69ae5662c9f2189416748ceb4
o0001 <70506909+o0001@users.noreply.github.com>
2026-07-21 10:03:23 +0900

    Update new.py

--- new.py
+++ new.py
@@ -15,11 +15,6 @@ def add_todo():
 
 @st.dialog("오늘의 다짐 수정")
 def edm():
-    motto = st.text_input("나의 한 줄 좌우명을 적어주세요")
-    if st.button("다짐저장"):
-        st.session_state.user_motto = motto
-        st.session_state.motto_updated = True
-        st.rerun()
 
 def page_motto():
     st.title("🌱 갓생 살기 플래너")
